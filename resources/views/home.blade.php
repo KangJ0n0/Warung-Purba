4f1dc7a76c2c42fdfea5fe00d62d589976b78643
Jono <127705274+KangJ0n0@users.noreply.github.com>
2024-05-16 23:56:50 +0700

    tambah navbar template

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+  @vite('resources/css/app.css')
+</head>
+<x-navbar></x-navbar>
+<body>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</body>
+</html>
